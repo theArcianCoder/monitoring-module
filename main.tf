f297801c381fc7d3be0c1ba374f2e3c82db3292e
theArcianCoder <countingcr223@gmail.com>
2023-08-18 16:16:36 +0530

    test changes

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ provider "kubernetes" {
     command     = "aws"
     args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
   }
-  host                   = local.cluster_endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
 
@@ -24,7 +23,6 @@ data "aws_eks_cluster_auth" "cluster" {
 
 module "kube_prometheus" {
   source         = "git::https://github.com/theArcianCoder/terraform-module-kube-prometheus.git"
-  eks_cluster_id = local.cluster_endpoint
   namespace      = var.namespace
   stack_name     = var.stack_name
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,6 @@
-locals {
-  desired_cluster_name = var.cluster_name  # Replace with your desired cluster name
-
-  cluster_endpoint = data.kubernetes_config.current_context.clusters[local.desired_cluster_name].cluster.server
-}
 
 variable "cluster_name" {
   description = "Name of the EKS cluster"
-}
-
-variable "cluster_endpoint" {
-  description = "Cluster endpoint URL"
 }
 
 variable "namespace" {
