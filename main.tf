a6c79a6c386f838493c0aeb9e02386f6ec0c1a6e
theArcianCoder <countingcr223@gmail.com>
2023-09-05 17:34:54 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -2,13 +2,6 @@ terraform {
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
-      host                   = data.aws_eks_cluster.cluster.endpoint
-      cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-      exec = {
-        api_version = "client.authentication.k8s.io/v1beta1"
-        command     = "aws"
-        args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-      }
     }
   }
 }
