dfa4760c0f7d3b5ffe15597065cb8b54da772cc6
theArcianCoder <countingcr223@gmail.com>
2023-09-05 16:51:50 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -20,6 +20,16 @@ provider "helm" {
     }
 }
 
+provider "kubectl" {
+      exec {
+        api_version = "client.authentication.k8s.io/v1beta1"
+        command     = "aws"
+        args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      }
+      host                   = data.aws_eks_cluster.cluster.endpoint
+      cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+}
+
 data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
