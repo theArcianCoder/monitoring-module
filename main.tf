b47b9781c414ec4da2cd2f602067af35818438c7
theArcianCoder <countingcr223@gmail.com>
2023-09-05 17:33:34 +0530

    Test Changes

--- main.tf
+++ main.tf
@@ -2,6 +2,13 @@ terraform {
   required_providers {
     kubectl = {
       source  = "gavinbunney/kubectl"
+      host                   = data.aws_eks_cluster.cluster.endpoint
+      cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+      exec = {
+        api_version = "client.authentication.k8s.io/v1beta1"
+        command     = "aws"
+        args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      }
     }
   }
 }
@@ -25,16 +32,6 @@ provider "helm" {
       host                   = data.aws_eks_cluster.cluster.endpoint
       cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     }
-}
-
-provider "kubectl" {
-      exec {
-        api_version = "client.authentication.k8s.io/v1beta1"
-        command     = "aws"
-        args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
-      }
-      host                   = data.aws_eks_cluster.cluster.endpoint
-      cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
 }
 
 data "aws_eks_cluster" "cluster" {
